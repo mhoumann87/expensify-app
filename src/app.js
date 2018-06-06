aade58021489aaeca1386022408364282485b3e5
Michael Houmann <michael@michael-h.dk>
2018-06-06 10:35:26 +0200

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -5,9 +5,6 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import {addExpense} from "./actions/expenses";
-import {setTextFilter} from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
