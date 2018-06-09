d62cfa3089e8d3e7d373cf48d2c719b5d85780b5
Michael Houmann <michael@michael-h.dk>
2018-06-09 09:53:54 +0200

    Set up test database enviroment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,39 +1,16 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyAGhsUI45xBEgBM2Alhwksb0t7VTTxnnt4",
-  authDomain: "expensify-249f6.firebaseapp.com",
-  databaseURL: "https://expensify-249f6.firebaseio.com",
-  projectId: "expensify-249f6",
-  storageBucket: "expensify-249f6.appspot.com",
-  messagingSenderId: "35207720975"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: "Michael Houmann",
-  age: 50,
-  isSingle: true,
-  location: {
-    city: "Herlufmagle",
-    contry: "Denmark",
-    street: "Birkevaenget"
-},
-
-});
-
-// database.ref().set("This is my data");
-
-database.ref("age").set({
-  age: 49
-});
-
-database.ref("location/city").set("Copenhagen");
-
-database.ref("attributes").set({
-  height: 182,
-  weight: 90
-})
+export {firebase, database as default};
